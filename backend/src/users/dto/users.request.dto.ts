a27a71132936b5764126a4e97633c9de1238848f
spadaritucks <thiago.spadari02@gmail.com>
2025-07-27 17:04:52 -0300

    Atualiza o DTO de requisição de usuários para incluir validações mais específicas, como mensagens de erro personalizadas para CPF e data de nascimento. Torna os campos de data de nascimento e telefone opcionais, e adiciona validação para o campo 'active'.

--- backend/src/users/dto/users.request.dto.ts
+++ backend/src/users/dto/users.request.dto.ts
@@ -1,33 +1,37 @@
-import { IsBoolean, isBoolean, IsDate, IsNotEmpty, IsOptional, IsString, Length, Matches, Min } from "class-validator"
-
-export class UsersRequestDTO {
-
-
-    @IsNotEmpty({ message: "O nome completo do usuario é obrigátorio!" })
+import {
+    IsBoolean,
+    IsDateString,
+    IsNotEmpty,
+    IsOptional,
+    IsString,
+    Length,
+    Matches,
+  } from "class-validator";
+  
+  export class UsersRequestDTO {
+    @IsNotEmpty({ message: "O nome completo do usuário é obrigatório!" })
     @IsString()
     @Length(8, 60)
-    fullName: string
-
-    @IsNotEmpty({ message: "O CPF é obrigátorio!" })
+    fullName: string;
+  
+    @IsNotEmpty({ message: "O CPF é obrigatório!" })
     @Matches(/^\d{3}\.\d{3}\.\d{3}\-\d{2}$/, {
-        message: "CPF Invalido!"
+      message: "CPF inválido! Use o formato 000.000.000-00",
     })
-    cpf: string
-
+    cpf: string;
+  
     @IsOptional()
-    @IsString()
-    birthDate: string
-
-
+    @IsDateString({}, { message: "Data de nascimento inválida. Use o formato YYYY-MM-DD." })
+    birthDate: string;
+  
     @IsOptional()
     @IsString()
     @Matches(/^\(?\d{2}\)?\s?\d{5}-\d{4}$/, {
-        message : "Telefone inválido. Use o formato (99) 99999-9999"
+      message: "Telefone inválido. Use o formato (99) 99999-9999",
     })
-    phone: string
-
-    @IsBoolean()
-    active: boolean
-
-
-}
+    phone: string;
+  
+    @IsBoolean({ message: "O campo 'active' deve ser verdadeiro ou falso." })
+    active: boolean;
+  }
+  
