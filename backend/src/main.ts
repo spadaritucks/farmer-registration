4eb03e687f08a2faa4c751ab0df146e9119b5e6a
spadaritucks <thiago.spadari02@gmail.com>
2025-07-27 17:00:04 -0300

    Adiciona configuração CORS para permitir requisições do frontend em http://localhost:3000.

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,6 +5,9 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+    app.enableCors({
+      origin : "http://localhost:3000"
+    })
 
   const config = new DocumentBuilder()
     .setTitle('Cadastro do Agricultor')
@@ -16,7 +19,6 @@ async function bootstrap() {
   SwaggerModule.setup('api', app, documentFactory);
 
   app.useGlobalPipes(new ValidationPipe())
-
   await app.listen(process.env.PORT ?? 8000);
 
 }
